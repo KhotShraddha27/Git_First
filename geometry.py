6da00d51df4bc2088be30efe3238e76ac7fc4409
Shraddha Khot <khotshraddha27@gmail.com>
2025-12-10 23:39:11 +0530

    rect_area_peri

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def calc_rect_area(l,b):
+    print("rect area: ",l*b)
+
+def calc_rect_perimerter(l,b):
+    print("rect area: ",2*(l+b))
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import arithmetic
+import geometry as geo
+from geometry import calc_rect_perimerter
 
 print("Hello World")
 
 arithmetic.add(9,9)
 arithmetic.sub(7,6)
+
+geo.calc_rect_perimerter(4,5)
+geo.calc_rect_area(4,5)
